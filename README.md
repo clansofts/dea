ffe2b7e8581d588a697901655b4199dfb0b48eee
Samuel Mwaura <5047331+clansofts@users.noreply.github.com>
2023-06-13 18:38:36 +0300

    Updated tests
    Updated README.md
    Added more tests.
    Grouped tests.

--- README.md
+++ README.md
@@ -2,3 +2,29 @@ double entry accounting command-line application with an entrypoint in `bin/`, l
 in `lib/`, and example unit test in `test/`.
 
 Find some introduction on double entry accounting concept documentation https://medium.com/p/bcc2bf2e78e2
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Acknowledgements
+
+ - [Double Entry Accounting](https://www.investopedia.com/terms/d/double-entry.asp)
+ - [Balanced Software](https://www.balanced.software/generate-reports-from-the-ledger/)
+ - [DEA - Relational Database](https://medium.com/@RobertKhou/double-entry-accounting-in-a-relational-database-2b7838a5d7f8)
+
+## Authors
+
+- [@clansofts](https://www.github.com/clansofts)
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://sotiwa.web.app/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/samuel-mwaura-14592131/)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/SammieMwaorer)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- test/dea_test.dart
+++ test/dea_test.dart
@@ -4,41 +4,80 @@ import 'package:test/test.dart';
 import 'package:uuid/uuid.dart';
 
 void main() {
-  test('double entry accounting ...', () async {
-    final cash = Uuid().v4();
-    final loan = Uuid().v4();
+  late Books books;
 
-    final b = Books();
+  setUp(() {
+    books = Books();
+  });
 
-    final amount = 10000.00;
-    final less = 10000.00;
-    final description = "KCB overdraft";
-    final merchantId = "Malipo Popote Solutions LLC";
+  group('double entry acounting features', () {
+    test('return merchant setup ...', () async {
+      //final b = Books();
+      final merchantId = "Malipo Popote Solutions LLC";
 
-    // creating accounts
-    b
-      //initialize
-      ..setupMerchant(name: "Malipo Popote Solutions LLC")
-      // adding accounts
-      ..addAccount(id: cash, accountType: AccountType.assets, name: "Cash")
-      ..addAccount(
-          id: loan, accountType: AccountType.liabilities, name: "Loans")
+      books.setupMerchant(name: merchantId);
+      final expected = books.getMerchant(merchantId: merchantId);
+      print(expected);
 
-      // transacting
-      ..transact(merchantId)
-      // adding all accounting entries required
-      ..debit(account: cash, amount: less, description: description)
-      //..debit(account: cash, amount: 0, description: "erroneous")
-      ..credit(account: loan, amount: amount, description: description)
-      // validating and commiting transaction
-      ..commit();
+      expect(expected.name, merchantId);
+    });
 
-    //geting accounts
-    //print(b.getAccounts());
-    //print(b.getAccountStatement(cash));
-    //print(b.getAccountStatement(loan));
-    // print(b.getAccountStatement());
-    // getting trial balances
-    print(b.getTrialBalances(merchantId: merchantId));
+    test('return account created ...', () async {
+      final cash = Uuid().v4();
+
+      books.addAccount(id: cash, accountType: AccountType.assets, name: "Cash");
+      final expected = books.getAccount(cash);
+      print(expected);
+
+      expect(expected.accountId, cash);
+    });
+
+    test('return multiple accounts created ...', () async {
+      final merchantId = "Malipo Popote Solutions LLC";
+      final cash = Uuid().v4();
+      final loan = Uuid().v4();
+
+      books
+        ..addAccount(id: cash, accountType: AccountType.assets, name: "Cash")
+        ..addAccount(
+            id: loan, accountType: AccountType.liabilities, name: "Loans");
+
+      final expected = books.getAccounts(merchantId: merchantId);
+      print(expected);
+
+      expect(expected.length, 2);
+    });
+
+    test('double entry accounting transaction ...', () async {
+      final cash = Uuid().v4();
+      final loan = Uuid().v4();
+
+      final amount = 10000.00;
+      final less = 10000.00;
+      final description = "KCB overdraft";
+      final merchantId = "Malipo Popote Solutions LLC";
+
+      // creating accounts
+      books
+        //initialize
+        ..setupMerchant(name: "Malipo Popote Solutions LLC")
+        // adding accounts
+        ..addAccount(id: cash, accountType: AccountType.assets, name: "Cash")
+        ..addAccount(
+            id: loan, accountType: AccountType.liabilities, name: "Loans")
+
+        // transacting
+        ..transact(merchantId)
+        // adding all accounting entries required
+        ..debit(account: cash, amount: less, description: description)
+        //..debit(account: cash, amount: 0, description: "erroneous")
+        ..credit(account: loan, amount: amount, description: description)
+        // validating and commiting transaction
+        ..commit();
+
+      final expected = books.getTrialBalances(merchantId: merchantId);
+
+      expect(expected.length, 2);
+    });
   });
 }
